86a8bbf05b2fee667a4ba135a7147c10d005b7c4
mohd ammar <ammarkhan575@gmail.com>
2026-03-06 16:30:30 +0530

    ADD: initialize configuration, server setup, and database migration; enhance .env example

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -1,11 +1,13 @@
 package config
 
 import (
+	"errors"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/go-playground/validator/v10"
-	_ "github.com/joho/godotenv/autoload"
+	"github.com/joho/godotenv"
 	"github.com/knadh/koanf/providers/env"
 	"github.com/knadh/koanf/v2"
 	"github.com/rs/zerolog"
@@ -27,9 +29,9 @@ type Primary struct {
 
 type ServerConfig struct {
 	Port               string   `koanf:"port" validate:"required"`
-	ReadTimeout        int      `koanf:"read_timeount" validate:"required"`
+	ReadTimeout        int      `koanf:"read_timeout" validate:"required"`
 	WriteTimeout       int      `koanf:"write_timeout" validate:"required"`
-	IdleTimeout        int      `koanf:"idle_timeoutn" validate:"required"`
+	IdleTimeout        int      `koanf:"idle_timeout" validate:"required"`
 	CORSAllowedOrigins []string `koanf:"cors_allowed_origins" validate:"required"`
 }
 
@@ -43,7 +45,7 @@ type DatabaseConfig struct {
 	MaxOpenConns    int    `koanf:"max_open_conns" validate:"required"`
 	MaxIdleConns    int    `koanf:"max_idle_conns" validate:"required"`
 	ConnMaxLifetime int    `koanf:"conn_max_lifetime" validate:"required"`
-	ConnMaxIdletime int    `koanf:"conn_max_idletime" validate:"required"`
+	ConnMaxIdletime int    `koanf:"conn_max_idle_time" validate:"required"`
 }
 
 type RedisConfig struct {
@@ -58,8 +60,38 @@ type AuthConfig struct {
 	SecretKey string `koanf:"secret_key" validate:"required"`
 }
 
+func loadDotEnv(logger *zerolog.Logger) {
+	workingDir, err := os.Getwd()
+	if err != nil {
+		logger.Warn().Err(err).Msg("could not determine working directory for .env lookup")
+		return
+	}
+
+	currentDir := workingDir
+	for {
+		envPath := filepath.Join(currentDir, ".env")
+		if _, statErr := os.Stat(envPath); statErr == nil {
+			if loadErr := godotenv.Load(envPath); loadErr != nil {
+				logger.Warn().Err(loadErr).Str("path", envPath).Msg("failed to load .env file")
+			}
+			return
+		} else if !errors.Is(statErr, os.ErrNotExist) {
+			logger.Warn().Err(statErr).Str("path", envPath).Msg("failed to check .env file")
+			return
+		}
+
+		parent := filepath.Dir(currentDir)
+		if parent == currentDir {
+			return
+		}
+
+		currentDir = parent
+	}
+}
+
 func LoadConfig() (*Config, error) {
 	logger := zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr}).With().Timestamp().Logger()
+	loadDotEnv(&logger)
 
 	k := koanf.New(".")
 
